3510bd003f2d7bc1b15840fcf16479fa46f9af75
William Alexander Matallana <wmatallana28@gmail.com>
2023-02-07 19:01:31 -0500

    Ajuste salida por consola

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,10 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
 
+        System.out.println("Bienvenidos a la clase de CP3");
+
+        System.out.println("En esta clase van a aprender Git");
+
 
 
 
